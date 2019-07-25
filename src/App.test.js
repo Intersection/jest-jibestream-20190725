487aef22c039d32873cfe1e7f4c873c44787d86a
Patrick Way <patrick.way@intersection.com>
2019-07-25 11:45:04 -0400

    Add jibestream libraries

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import jmap from 'jmap.js'
+import MapUIKit from '@jibestream-dev/jmap-mapui-kit'
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
